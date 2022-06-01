2cb20392a65ec9c7d958bed9e696cd66f9b8c63d
Mile S <81876481+ccadevs@users.noreply.github.com>
2022-06-01 21:19:34 +0200

    Create nav.php

---
+++ views/nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<button aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarTogglerDemo01" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
+	<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item>">
+                <a class="nav-link" href="https://example.com/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://example.com/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://example.com/database/">Our records</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://example.com/contact">Contact</a>
+            </li>
+        </ul>
+	</div>
+</nav>
